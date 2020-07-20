118b6cbd2c1960a9763223a02a680cdea963f011
datstarkey <jakestarkey@gmail.com>
2020-07-20 11:39:52 +0100

    Add Hotjar

--- main-website/src/main.ts
+++ main-website/src/main.ts
@@ -3,6 +3,17 @@ import App from "./App.vue";
 import "animate.css";
 import VueObserveVisibility from "vue-observe-visibility";
 
+import Hotjar from "vue-hotjar";
+
+//checks if we're in production
+const isProduction = process.env.NODE_ENV == "production";
+
+Vue.use(Hotjar, {
+  id: "1908646",
+  snippetVersion: 6,
+  isProduction: isProduction,
+});
+
 Vue.use(VueObserveVisibility);
 
 Vue.config.productionTip = false;
